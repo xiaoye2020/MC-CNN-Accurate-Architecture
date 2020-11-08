ad6d7caac67238a1e2e6a5cbe4de0b43052cb34c
xiaoye2020 <67257075+xiaoye2020@users.noreply.github.com>
2020-11-08 15:40:28 +0800

    1108

--- mian.py
+++ mian.py
@@ -48,18 +48,89 @@ if args.loadmodel is not None:
 
 optimizer = optim.SGD(model.parameters(), lr=0.003, betas=(0.9, 0.999))
 
+def train(imgL,imgR,disp_L):
+        model.train()
+        #---------
+        mask = (disp_true > 0)
+        mask.detach_()
+        #----
 
+        optimizer.zero_grad()
+        output = model(imgL,imgR)
+        output = torch.squeeze(output,1)
+        loss = nn.BCELoss(output[mask], disp_true[mask], size_average=True)
 
+        loss.backward()
+        optimizer.step()
 
+        return loss.data[0]
 
+def test(imgL,imgR,disp_true):
+        model.eval()
+        imgL   = Variable(torch.FloatTensor(imgL))
+        imgR   = Variable(torch.FloatTensor(imgR))   
+        if args.cuda:
+            imgL, imgR = imgL.cuda(), imgR.cuda()
 
+        with torch.no_grad():
+            output3 = model(imgL,imgR)
 
+        pred_disp = output3.data.cpu()
 
+        #computing 3-px error#
+        true_disp = disp_true
+        index = np.argwhere(true_disp>0)
+        disp_true[index[0][:], index[1][:], index[2][:]] = np.abs(true_disp[index[0][:], index[1][:], index[2][:]]-pred_disp[index[0][:], index[1][:], index[2][:]])
+        correct = (disp_true[index[0][:], index[1][:], index[2][:]] < 3)|(disp_true[index[0][:], index[1][:], index[2][:]] < true_disp[index[0][:], index[1][:], index[2][:]]*0.05)      
+        torch.cuda.empty_cache()
 
+        return 1-(float(torch.sum(correct))/float(len(index[0])))
 
 def main():
+	max_acc=0
+	max_epo=0
+	start_full_time = time.time()
+
+	for epoch in range(1, args.epochs+1):
+	   total_train_loss = 0
+	   total_test_loss = 0
+	   adjust_learning_rate(optimizer,epoch)
+           
+               ## training ##
+           for batch_idx, (imgL_crop, imgR_crop, disp_crop_L) in enumerate(TrainImgLoader):
+               start_time = time.time() 
+
+               loss = train(imgL_crop,imgR_crop, disp_crop_L)
+	       print('Iter %d training loss = %.3f , time = %.2f' %(batch_idx, loss, time.time() - start_time))
+	       total_train_loss += loss
+	   print('epoch %d total training loss = %.3f' %(epoch, total_train_loss/len(TrainImgLoader)))
+	   
+               ## Test ##
+
+           for batch_idx, (imgL, imgR, disp_L) in enumerate(TestImgLoader):
+               test_loss = test(imgL,imgR, disp_L)
+               print('Iter %d 3-px error in val = %.3f' %(batch_idx, test_loss*100))
+               total_test_loss += test_loss
 
 
+	   print('epoch %d total 3-px error in val = %.3f' %(epoch, total_test_loss/len(TestImgLoader)*100))
+	   if total_test_loss/len(TestImgLoader)*100 > max_acc:
+		max_acc = total_test_loss/len(TestImgLoader)*100
+		max_epo = epoch
+	   print('MAX epoch %d total test error = %.3f' %(max_epo, max_acc))
+
+	   #SAVE
+	   savefilename = args.savemodel+'finetune_'+str(epoch)+'.tar'
+	   torch.save({
+		    'epoch': epoch,
+		    'state_dict': model.state_dict(),
+		    'train_loss': total_train_loss/len(TrainImgLoader),
+		    'test_loss': total_test_loss/len(TestImgLoader)*100,
+		}, savefilename)
+	
+        print('full finetune time = %.2f HR' %((time.time() - start_full_time)/3600))
+	print(max_epo)
+	print(max_acc)
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -5,14 +5,82 @@ import torch.utils.data
 from torch.autograd import Variable
 import torch.nn.functional as F
 import math
-from submodule import *
+import torch.optim as optim
+
+
+
+def train(imgL,imgR):
+        model.train()
+        #---------
+        # mask = (disp_true > 0)
+        # mask.detach_()
+        #----
+
+        optimizer.zero_grad()
+        output = model(imgL,imgR)
+        output = torch.squeeze(output,0)
+
+        test=torch.zero_(1)
+        test= Variable(torch.FloatTensor(test))
+        loss = nn.BCELoss(output3, dtest, size_average=True)
+
+        loss.backward()
+        optimizer.step()
+
+        return loss.data[0]
+
 
 class MCCNNNet(nn.Module):
-    def __init__(self, maxdisp):
+    def __init__(self):
         super(MCCNNNet, self).__init__()
-        
+        self.feature_extraction=feature_extraction()
+        self.layer1 = nn.Sequential(nn.Linear(9*9*112*2,384), nn.ReLU(inplace=True))
+        self.layer2 = nn.Sequential(nn.Linear(384,384), nn.ReLU(inplace=True))
+        self.layer3 = nn.Sequential(nn.Linear(384,384), nn.ReLU(inplace=True))
+        self.layer4 = nn.Linear(384,1)
 
 
     def forward(self, left, right):
+        refimg_fea     = self.feature_extraction(left)
+        targetimg_fea  = self.feature_extraction(right)
+        left_linear=refimg_fea.view([refimg_fea.size()[0],-1])
+        right_linear=targetimg_fea.view([refimg_fea.size()[0],-1])
 
+        linear_input=torch.cat([left_linear,right_linear],dim=1)
+        output=self.layer1(linear_input)
+        output=self.layer2(output)
+        output=self.layer3(output)
+        output=self.layer4(output)
+        output=F.sigmoid(output)
+        return output
+
+
+class feature_extraction(nn.Module):
+    def __init__(self):
+        super(feature_extraction, self).__init__()
+        self.layer1 = nn.Sequential(nn.Conv2d(in_channels=1,out_channels=112,kernel_size=3,stride=1,padding=1), nn.ReLU(inplace=True))
+        self.layer2 = nn.Sequential(nn.Conv2d(in_channels=112,out_channels=112,kernel_size=3,stride=1,padding=1), nn.ReLU(inplace=True))
+        self.layer3 = nn.Sequential(nn.Conv2d(in_channels=112,out_channels=112,kernel_size=3,stride=1,padding=1), nn.ReLU(inplace=True))
+        self.layer4 = nn.Sequential(nn.Conv2d(in_channels=112,out_channels=112,kernel_size=3,stride=1,padding=1), nn.ReLU(inplace=True))
+       
+
+    def forward(self, image):
+        output=self.layer1(image)
+        output=self.layer2(output)
+        output=self.layer3(output)
+        output=self.layer4(output)
         
+        return output
+  
+
+model=MCCNNNet()
+
+imgL=torch.rand(1,1,9, 9)
+imgL= Variable(torch.FloatTensor(imgL))
+
+imgR=torch.rand(1,1,9, 9)
+imgR= Variable(torch.FloatTensor(imgR))
+
+optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
+
+train(imgL,imgR)
